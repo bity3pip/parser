f600fccc5f03f5e5befe8c30b284fb00bca090ad
bity3pip <egor.sh23@gmail.com>
2025-01-08 14:11:08 +0100

    fix bug, parse one link

---
+++ crunchbase_parser/api/app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from crunchbase_parser.parser.selenium_parser import WebParserSelenium
+
+app = FastAPI()
+
+
+class UrlsRequest(BaseModel):
+    urls: List[str]
+
+
+@app.post("/parse/")
+async def parse_urls(request: UrlsRequest):
+    parser = WebParserSelenium()
+
+    results = {}
+    for url in request.urls:
+        data = parser.fetch_data(url)
+        results[url] = data
+
+    return {"results": results}
---
+++ crunchbase_parser/parser/selenium_parser.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class WebParserSelenium:
+    def __init__(self):
+        self.driver = None
+
+    def _initialize_driver(self):
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--ignore-certificate-errors")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+    def fetch_data(self, url):
+        try:
+            print(f"Запрос к URL: {url}")
+            if not self.driver:
+                self._initialize_driver()
+
+            self.driver.get(url)
+
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.TAG_NAME, "chips-container"))
+            )
+
+            page_source = self.driver.page_source
+            soup = BeautifulSoup(page_source, "html.parser")
+
+            chips_container = soup.find("chips-container")
+            if chips_container:
+                chip_texts = chips_container.find_all("div", class_="chip-text")
+                if chip_texts:
+                    return [chip.text.strip() for chip in chip_texts]
+            return ["Индустрия не найдена"]
+        except Exception as e:
+            print(f"Ошибка при парсинге {url}: {str(e)}")
+            return f"Ошибка при парсинге {url}: {str(e)}"
+        finally:
+            if self.driver:
+                self.driver.quit()
